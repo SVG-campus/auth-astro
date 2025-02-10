ee7a2ab06abec26b16e25fd751a76c64203abe2b
Santiago de Jesus Villalobos-Gonzalez <svillalobos-gonzalez@my.campus.edu>
2025-02-10 22:19:00 +0000

    COMMIT

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        '~': path.resolve('./src'),
-        'astrowind:config': path.resolve('./src/config/astrowind.config.js'),
-        'auth:config': path.resolve('./src/config/auth.config.ts'),
+        '~': path.resolve(__dirname, './src'),
+        // Alias "tailwindcss/nesting" to our empty stub file
+        'tailwindcss/nesting': path.resolve(__dirname, 'src/empty.js'),
       },
     },
   },
